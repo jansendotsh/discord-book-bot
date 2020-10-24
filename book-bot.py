ed214191b6737e4540c38ac5457e35d98829a767
jansendotsh <garrett@jansen.sh>
2020-10-24 10:32:39 -0700

    First commit

--- book-bot.py
+++ book-bot.py
@@ -6,5 +6,9 @@ discordToken = os.getenv('DISCORD_TOKEN')
 client = discord.Client()
 
 # Insert events here
+@client.event
+async def on_message(message):
+    if message.content == "fuck":
+        await message.channel.send("NOT INDOORS!")
 
 client.run(discordToken)
