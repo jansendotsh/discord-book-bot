ee551122b6f5f86e1e9b98122b28547768f8ae94
jansendotsh <garrett@jansen.sh>
2020-11-09 17:42:32 -0800

    New Google Drive test

--- gdrive-test.py
+++ gdrive-test.py
@@ -1,34 +1,25 @@
 # toDo
 # Make work in Discord bot events
 
-import os
-import gspread
+import os, gspread, random
 from oauth2client.service_account import ServiceAccountCredentials
 
 def main():
     # Open the Google Sheet with book data
     gc = gspread.service_account(filename='key.json')
     sheet = gc.open_by_key("1VoR6d3Vwbhk5sxh18jUwtSZP3hr_hBjUn2CDSmBQzus")
+    
+    upcoming = sheet.get_worksheet(3).get_all_records()
+    lengthUp = len(upcoming)
+    randoBook = random.randint(2,lengthUp+1)
+    newNext = sheet.get_worksheet(3).row_values(randoBook)
 
-    def progress(username, progress):
-        progSheet = sheet.get_worksheet(3)
-        pageCount = int(progSheet.cell(1, 3).value)
+    print(newNext)
+    print(randoBook)
 
-        try:
-            curEntry = progSheet.find(username)
-            progCell = curEntry.col + 1
+    sheet.get_worksheet(3).insert_row(newNext,2)
+    sheet.get_worksheet(3).delete_rows(randoBook+1)
 
-            if progress[-1] == "%":
-                progSheet.update_cell(curEntry.row, progCell, progress)
-            else:
-                progress = int(progress)
-                progress = str(int((progress/pageCount)*100))
-                progSheet.update_cell(curEntry.row, progCell, progress+"%")
-
-        except gspread.exceptions.CellNotFound:
-            progSheet.append_row([username, progress])
-
-    progress("frailtyy#7182","222")
 
 if __name__ == '__main__':
     main()
